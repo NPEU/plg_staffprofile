340917dbf5e9b6241cb5dc728b68f9f148eecc54
Andy Kirk <andy@andykirk.net>
2025-02-18 16:23:34 +0000

    Fix $form check.

--- src/Extension/StaffProfile.php
+++ src/Extension/StaffProfile.php
@@ -243,8 +243,7 @@ class StaffProfile extends CMSPlugin implements SubscriberInterface
     {
         [$form, $data] = array_values($event->getArguments());
 
-        if ($form && !($form instanceof \Joomla\CMS\Form\Form)) {
-            throw new GenericDataException(Text::_('JERROR_NOT_A_FORM'), 500);
+        if (!$form instanceof \Joomla\CMS\Form\Form) {
             return;
         }
 
